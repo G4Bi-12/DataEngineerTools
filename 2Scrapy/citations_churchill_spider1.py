6a2d28ef77de5eb100c94a58a281b5465dae1104
cochardg <gabriel.cochard@edu.esiee.fr>
2024-12-03 16:36:29 +0100

    Big Commit

--- 2Scrapy/citations_churchill_spider1.py
+++ 2Scrapy/citations_churchill_spider1.py
@@ -5,6 +5,10 @@ class ChurchillQuotesSpider(scrapy.Spider):
     start_urls = ["http://evene.lefigaro.fr/citations/winston-churchill",]
 
     def parse(self, response):
-        for cit in response.xpath('//div[@class="figsco__quote__text"]'):
-            text_value = cit.xpath('a/text()').extract_first()
-            yield { 'text' : text_value }
+        for cit in response.xpath('//li[@class="figsco__selection__list__evene__list__item"]'):
+            
+            text_value = cit.xpath('//div[@class="figsco__quote__text"]').xpath('a/text()').extract_first()
+            if text_value:
+                text_value = text_value.replace('“','').replace('”','')
+            author_value = cit.xpath('//div[@class="figsco__fake__col-9"]').xpath('a/text()').extract_first()
+            yield {'text': text_value, 'author': author_value}
